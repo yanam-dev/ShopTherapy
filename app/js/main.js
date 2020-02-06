5d40ff6f4792e57bc5ed869b0c5f2822b707976b
yanam-dev <yamusjko@gmail.com>
2020-02-06 15:12:10 +0100

    added slider stores section, stores page

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,67 @@ $(function(){
         speed: 800,
         slidesToShow: 1
     });
+// WORKS slider start
+    var sickPrimary = {
+        autoplay: true,
+        autoplaySpeed: 2400,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        speed: 1800,
+        cssEase: 'cubic-bezier(.84, 0, .08, .99)',
+        asNavFor: '.text-slider',
+        centerMode: true,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next')
+    }
+
+    var sickSecondary = {
+        autoplay: true,
+        autoplaySpeed: 2400,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1800,
+        cssEase: 'cubic-bezier(.84, 0, .08, .99)',
+        asNavFor: '.image-slider',
+        prevArrow: $('.prev'),
+        nextArrow: $('.next')
+    }
+
+    $('.image-slider').slick(sickPrimary);
+    $('.text-slider').slick(sickSecondary);
+// WORKS slider end
+
+        // menu button start
+        const navSLide = () => {
+            const menu__btn = document.querySelector('.menu__btn');
+            const nav = document.querySelector('.nav-menu');
+            const navLinks = document.querySelectorAll('.nav-menu > li');
+            
+            menu__btn.addEventListener('click', () => {
+                // Toggle Nav
+                nav.classList.toggle('menu-active')
+        
+                // Animate Links
+                navLinks.forEach((link, index) => {
+                    if (link.style.animation) {
+                        link.style.animation = '';
+                    } else {
+                        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                    }
+                });
+                // Burger Animation
+                menu__btn.classList.toggle('toggle');
+            });
+        }
+        navSLide(); 
+        // menu button end
+
+// Smooth scrolling
+$("nav li a").click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    $("body,html").animate({scrollTop: destination }, 1000);
+});
+
 
 });
