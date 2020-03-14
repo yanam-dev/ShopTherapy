43ad63bbdf41132aa4b308d118aed35dbc1c7754
yanam-dev <yamusjko@gmail.com>
2020-03-14 19:30:34 +0100

    adapted main slider, fixed errors on stores page

--- app/js/main.js
+++ app/js/main.js
@@ -31,9 +31,25 @@ $(function(){
         prevArrow: $('.prev'),
         nextArrow: $('.next')
     }
+ var sickThirdly = {
+        autoplay: true,
+        autoplaySpeed: 2400,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1800,
+        cssEase: 'cubic-bezier(.84, 0, .08, .99)',
+        asNavFor: '.text-slider',
+        centerMode: true,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next')
+    }
 
     $('.image-slider').slick(sickPrimary);
     $('.text-slider').slick(sickSecondary);
+    $('.image-slider--small').slick(sickThirdly);
+
+
+    
 // WORKS slider end
 
         // menu button start
